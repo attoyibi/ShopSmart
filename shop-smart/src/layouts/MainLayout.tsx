a74ae3a74cb2a097fe8e93b45b197bfa4a7f0eb8
attoyibi <43211768+attoyibi@users.noreply.github.com>
2024-10-10 08:52:28 +0700

    Update MainLayout.tsx
    make interface  for  children

--- shop-smart/src/layouts/MainLayout.tsx
+++ shop-smart/src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 // src/layouts/MainLayout.js
-import React from 'react';
+import React, { ReactNode } from 'react';
 import Navbar from '../components/Navbar'; // Komponen Navbar
 import Footer from '../components/Footer'; // Komponen Footer
-
-const MainLayout = ({ children }) => {
+interface MainLayoutProps {
+    children: ReactNode; // Define the type for children
+}
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     return (
         <div>
             <Navbar />
