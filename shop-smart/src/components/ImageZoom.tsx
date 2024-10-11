a51d616a05dae05583978f3c7619a89533bc5c72
attoyibi <43211768+attoyibi@users.noreply.github.com>
2024-10-11 12:58:20 +0700

    Update ImageZoom.tsx

--- shop-smart/src/components/ImageZoom.tsx
+++ shop-smart/src/components/ImageZoom.tsx
@@ -1,37 +1,37 @@
 "use client"
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 // Komponen untuk efek zoom pada gambar
-const ImageZoom = ({ product }) => {
+const ImageZoom = () => {
     const imgRef = useRef(null); // Membuat referensi ke elemen gambar
-    console.log("prduct dalam image = " + product)
+    // console.log("prduct dalam image = " + product)
     // Effect untuk menangani mouse move dan mouse leave
     useEffect(() => {
-        const img = imgRef.current;
-        console.log("imgRef.current = " + product)
+        // const img = imgRef.current;
+        // console.log("imgRef.current = " + product)
 
-        const handleMouseMove = (e) => {
-            const { left, top, width, height } = img.getBoundingClientRect();
-            const x = e.clientX - left;
-            const y = e.clientY - top;
+        // const handleMouseMove = (e) => {
+        //     const { left, top, width, height } = img.getBoundingClientRect();
+        //     const x = e.clientX - left;
+        //     const y = e.clientY - top;
 
-            // Mengatur origin zoom
-            img.style.transformOrigin = `${(x / width) * 100}% ${(y / height) * 100}%`;
-            img.classList.add('zoomed'); // Tambahkan class zoomed untuk efek
-        };
+        //     // Mengatur origin zoom
+        //     img.style.transformOrigin = `${(x / width) * 100}% ${(y / height) * 100}%`;
+        //     img.classList.add('zoomed'); // Tambahkan class zoomed untuk efek
+        // };
 
-        const handleMouseLeave = () => {
-            img.classList.remove('zoomed'); // Hapus class zoomed
-        };
+        // const handleMouseLeave = () => {
+        //     img.classList.remove('zoomed'); // Hapus class zoomed
+        // };
 
-        img.addEventListener('mousemove', handleMouseMove);
-        img.addEventListener('mouseleave', handleMouseLeave);
+        // img.addEventListener('mousemove', handleMouseMove);
+        // img.addEventListener('mouseleave', handleMouseLeave);
 
-        // Clean up event listener saat komponen di-unmount
-        return () => {
-            img.removeEventListener('mousemove', handleMouseMove);
-            img.removeEventListener('mouseleave', handleMouseLeave);
-        };
+        // // Clean up event listener saat komponen di-unmount
+        // return () => {
+        //     img.removeEventListener('mousemove', handleMouseMove);
+        //     img.removeEventListener('mouseleave', handleMouseLeave);
+        // };
     }, []);
 
     return (
